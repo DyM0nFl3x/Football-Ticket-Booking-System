e8fae0687b7a43c42ec3d5ebfe9f8aec3406d584
DyM0nFl3x <rakibhossain4230@gmail.com>
2026-07-17 12:23:47 +0600

    booking update

--- QUERY.sql
+++ QUERY.sql
@@ -49,20 +49,20 @@ CREATE TABLE Matches (
 -- 3. CREATE BOOKINGS TABLE
 -- =========================================================================
 CREATE TABLE Bookings (
-    booking_id TYPE,
-    user_id TYPE,
-    match_id TYPE,
-    seat_number TYPE,
-    payment_status TYPE,
-    total_cost TYPE,
-    
-    -- Write your constraint to make 'booking_id' the Primary Key
-    -- Write your Foreign Key constraint linking 'user_id' to the Users table
-    -- Write your Foreign Key constraint linking 'match_id' to the Matches table
-    -- Write your check constraint to ensure 'total_cost' is non-negative
-    -- Write your check constraint to restrict 'payment_status' values
+  booking_id SERIAL PRIMARY KEY,
+  user_id INT NOT NULL REFERENCES Users (user_id),
+  match_id INT NOT NULL REFERENCES Matches (match_id),
+  seat_number VARCHAR(20),
+  payment_status VARCHAR(40) CHECK (
+    payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
+  ),
+  total_cost DECIMAL(10, 2) NOT NULL CHECK (total_cost >= 0)
+  --(done) Write your constraint to make 'booking_id' the Primary Key
+  --(done) Write your Foreign Key constraint linking 'user_id' to the Users table
+  --(done) Write your Foreign Key constraint linking 'match_id' to the Matches table
+  --(done) Write your check constraint to ensure 'total_cost' is non-negative
+  --(done) Write your check constraint to restrict 'payment_status' values
 );
-
 
 -- =========================================================================
 -- DATA SEEDING: INSERT SAMPLE DATA INTO USERS
